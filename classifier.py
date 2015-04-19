d8f500edff897b6165d69fa2b9a9d639333586a6
Betty <betty.leung2@mail.mcgill.ca>
2015-04-19 17:10:38 -0400

    precision recall accuracy added, run all features

--- classifier.py
+++ classifier.py
@@ -6,20 +6,20 @@ from sys import getsizeof
 # BL: This function creates list of features from string
 def features(s, numResults, uniGT, biGT, triGT, quadGT):
     
-##    bag=(bagofwords(s, numResults))
-##    #print bag
-##    bagout=""
-##    for n in range(numResults):
-##        bagout+=",'freqword"+str(n)+"':bag["+str(n)+"]"
-##    print bagout
-    #[u, b, t, q]=gettextGT([s], uniGT, biGT, triGT, quadGT)
+    bag=(bagofwords(s, numResults))
+    #print bag
+    bagout=""
+    for n in range(numResults):
+        bagout+=",'freqword"+str(n)+"':bag["+str(n)+"]"
+    print bagout
+    [u, b, t, q]=gettextGT([s], uniGT, biGT, triGT, quadGT)
     out="{'avg sent len': aveSenLen(s),'avg sent sd':senLenVar(s)"
-    #out+=",'avg word len':aveWordLen(s),'avg word sd':wordlengthSD(s)"
+    out+=",'avg word len':aveWordLen(s),'avg word sd':wordlengthSD(s)"
     out+=",'type-token ratio':getAveTTR(s, 100)"
-    #out+=",'unigram log-probs':u,'bigram log-probs':b,'quadgram log-probs':q"
-    #out+=",'trigram log-probs':t"
-##    out+=","
-##    out+=bagout[1:]
+    out+=",'unigram log-probs':u,'bigram log-probs':b,'quadgram log-probs':q"
+    out+=",'trigram log-probs':t"
+    out+=","
+    out+=bagout[1:]
     out+="}"
     try:
         features=eval(out)
@@ -49,20 +49,69 @@ def makeFeatureList(filelist, training_set, test_set):
     return training_feature_set, test_feature_set
 
 ##### Metrics #####
-def accuracy():
-    return 1
-def perplexity():
-    return 1
-def precision():
-    return 1
-def recall():
-    return 1
+##Input: test set, author-idx vector, test-set-idx, author that is being identified,
+##Output: Accuracy, precision, recall
+def test_metrics(test, author, test_set_idx, pickedAuthor):
+    classifier_output=[]
+    actual_answer=[]
+    for t in range(len(test)): #make sure size of test isn't 1
+        classifier_output.append(classifier.classify(test[t]))
+        actual_answer.append(author[test_set_idx[t]])
+
+    #In lsit f actual authors, replace non-Author with 'Other'
+    for a in range(len(actual_answer)):
+        if actual_answer[a]!=pickedAuthor:
+            actual_answer[a]='Other'
+    print actual_answer
+    print classifier_output
+
+    #initialize T/F-pos/negative values
+    correct=0
+    T_pos=0
+    T_neg=0
+    F_pos=0
+    F_neg=0
+    #count  T/F-pos/negative values
+    for a in range(len(actual_answer)):
+        if actual_answer[a]==classifier_output[a]:
+            correct+=1
+        if actual_answer[a]==pickedAuthor:
+            if classifier_output[a]==pickedAuthor:
+                T_pos+=1
+            elif classifier_output[a]=='Other':
+                F_neg+=1
+        elif actual_answer[a]=='Other':
+            if classifier_output[a]==pickedAuthor:
+                F_pos+=1
+            elif classifier_output[a]=='Other':
+                T_neg+=1
+
+    #calculate precision, accuracy, recall
+    accuracy=float(correct)/len(actual_answer)   
+    #precision
+    #relevant records retrieved/number of relevant and irrelevant records retrieved
+    #Of the clssifier outputs that are labelled Author X, how many are actually Author X?
+    precision=float(T_pos)/(T_pos+F_pos)
+    #recall
+    #relevnt records retrieved/relevant records retrieved or not retrieved
+    recall=float(T_pos)/(T_pos+F_neg)
+
+    #print warnings
+    if F_pos==0:
+        print 'Warning: No false positives'
+    if T_pos==0:
+        print 'Warning: No true positives'
+    if F_neg==0:
+        print 'Warning: No false negatives'
+    if T_neg==0:
+        print 'Warning: No true negatives'
+    return accuracy, precision, recall
 
 
 ##### Main #####
 
 # cd to dir, load all texts
-f=open('database.txt')
+f=open('db.txt')
 data=eval(f.read())
 
 #split data into author and text
@@ -72,7 +121,6 @@ text=[]
 title=[]
 for i in range(len(data)):
     #format: 'title', 'author', 'gutenberg#', '', 'text'
-    #title.append(data[i][0]) #not neccessary
     currauthor=data[i][1]
     author.append(currauthor)
     if currauthor in booksDict.keys():
@@ -81,15 +129,11 @@ for i in range(len(data)):
         booksDict[currauthor]=[i]
     text.append(data[i][4]) #next step is to strip table of contents and 'End of the Project Gutenberg EBook of '... from the end
 
-
-#random shuffle to pick author for training set
-
-
-#randomly pick sme number of books from other authors for training set and test set
+#randomly pick n number of books from other authors for training set and test set
 otherlist=[]
 num=0
 sizeIdx=0
-setlength=4
+setlength=8
 
 randNums=range(len(data))
 random.shuffle(randNums)
@@ -117,12 +161,12 @@ for j in range(setlength/2):
 Author_train=Author_train[:(setlength/2)]
 
 #compile list of test authors
-test_set=Author_test+non_author_test
-random.shuffle(test_set)
+test_set_idx=Author_test+non_author_test
+random.shuffle(test_set_idx)
 
 
 ### DEBUG ### Shortens runtime so that n-grams are calculated based on 1 text only
-Author_train=[Author_train[0]]
+#Author_train=[Author_train[0]]
 
 #compile text training set
 labelled_books_author = ([(text[i], pickedAuthor) for i in Author_train])
@@ -131,7 +175,7 @@ training_set=labelled_books_author + labelled_books_other
 random.shuffle(training_set)
 print 'Finished training set'
 #compile text test set
-test_set = ([(text[i]) for i in test_set])
+test_set = ([(text[i]) for i in test_set_idx])
 
 
 ## Calculate GTprob for current author' texts (needed to calculate GTProbs of individual texts)
@@ -149,11 +193,11 @@ classifier = nltk.NaiveBayesClassifier.train(train)
 #print most informative features
 print 'Most informative Features'
 print classifier.most_informative_features(5)
-
-##test classifier
-classifier.classify(test[0])
-
-
+#Evaluation
+[accuracy, precision, recall]=test_metrics(test, author, test_set_idx, pickedAuthor)
+print 'accuracy='+str(accuracy)
+print 'precision='+str(precision)
+print 'recall='+str(recall)
 
 
 
